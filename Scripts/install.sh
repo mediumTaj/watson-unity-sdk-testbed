53b7f09e850cd085af169e0b8aab07e83fcff4b5
Ajiemar Santiago <adsantiag@us.ibm.com>
2016-05-02 18:11:17 -0500

    edit install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -9,19 +9,19 @@ download() {
   url="$BASE_URL/$HASH/$package"
 
   echo "Downloading from $url: "
-  curl -o 'basename "$package"' "$url"
+  curl -o `basename "$package"` "$url"
 }
 
 install() {
   package=$1
   download "$package"
 
-  echo "Installing "'basename "$package"'
-  sudo installer -dumplog -package 'basename "$package"' -target /
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
 }
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
-# of available packages, where PLATFORM is 'osx' or 'win'
+# of available packages, where PLATFORM is `osx` or `win`
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
