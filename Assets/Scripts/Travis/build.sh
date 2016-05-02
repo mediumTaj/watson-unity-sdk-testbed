6b582c6363c1d1cb01732db2ab276c87b1cff7f1
Ajiemar Santiago <adsantiag@us.ibm.com>
2016-05-02 13:41:41 -0500

    initialized travis at project level

--- Assets/Scripts/Travis/build.sh
+++ Assets/Scripts/Travis/build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-project="watson-developer-cloud-unity-sdk"
+project="watson-developer-cloud-unity-sdk-testbed"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
